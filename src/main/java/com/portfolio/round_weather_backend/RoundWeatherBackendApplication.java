2e5d58ffc7205d6c9f7cdcc8b3cef9b4c4f9f4ef
cloudwi <cloudwi@naver.com>
2022-11-17 23:56:40 +0900

    first commit

---
+++ src/main/java/com/portfolio/round_weather_backend/RoundWeatherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.round_weather_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundWeatherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoundWeatherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/round_weather_backend/RoundWeatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.round_weather_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundWeatherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
